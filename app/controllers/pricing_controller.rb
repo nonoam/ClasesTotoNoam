c03fc15f6fbf67e86389649fef6fa4ed6fd46062
noamsito <vieiranoam94@gmail.com>
2025-09-01 23:08:31 -0400

    reset

--- app/controllers/pricing_controller.rb
+++
@@ -1,8 +0,0 @@
-class PricingController < ApplicationController
-  def index
-    @individual_price = Booking::INDIVIDUAL_PRICE
-    @group_price = Booking::GROUP_PRICE_PER_PERSON
-    @minimum_group = Booking::MINIMUM_GROUP_SIZE
-    @subjects = Subject.all.group_by(&:category)
-  end
-end
